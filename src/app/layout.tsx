b452fdd981e9ce11985f83bcc3e205bdc047c5e0
aqizakana <archifish56@gmail.com>
2024-11-10 18:30:13 +0900

    fix Homeのcssを設定し直し。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
-
-const metadata: Metadata = {
-  title: 'Frozen',
-  description: '開発中',
-  viewport: 'width=device-width, initial-scale=1',
-};
+ export const metadata: Metadata = {
+    title: 'bukubuku',
+    description: 'テキストメッセージから、抽象的な3Dオブジェクトを生成し、共有するアプリケーションです。海の中でのダイビングして、メッセージを送り合いましょう。',
+  }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+
   return (
     <html lang='ja'>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
 import BaseLayout from './baseLayout';
-import type { Metadata } from 'next'
+import Link from 'next/link';
+import styles from './page.module.css';
 
-export const metadata: Metadata = {
-  title: 'Scuba',
-  description: 'テキストメッセージから、抽象的な3Dオブジェクトを生成し、共有するアプリケーションです。海の中でのダイビングして、メッセージを送り合いましょう。',
-}
 
 export default function Home() {
   return (
-    <BaseLayout>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
     
+    <BaseLayout>
+      <div className={styles.container}>
+        <h1 className={styles.title} >bukubuku</h1>
+        <p className={styles.paragraph}>海では、“ブイ”で話す。<br/>     
+          初めての方はこちら。→
+          <Link href="/accounts" className={styles.link}>
+            登録/ログイン
+          </Link>
+          <br/>
+          登録済みの方はこちら。→
+          <Link href="/three" className={styles.link}>
+            canvasページ
+          </Link>
+        </p>
+    
+      </div>
     </BaseLayout>
   );
 }
