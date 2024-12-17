43285d5c37f8e127b7edbe650038cffa403775ce
aqizakana <archifish56@gmail.com>
2024-12-18 00:21:05 +0900

    fix Loadingイベント調整

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,9 +1,18 @@
 import styles from "./loading.module.css";
 
+
+
 export function Loading() {
 	return (
-		<div className={styles.head}>
-			<h1 className={styles.head__h1}>投稿中...ちょっち待ってね。</h1>
+		<div className={styles.cube_container}>
+			<div className={styles.cube}>
+				<div className={`${styles.face} ${styles.front}`}>投稿中</div>
+				<div className={`${styles.face} ${styles.back}`}>投稿中</div>
+				<div className={`${styles.face} ${styles.right}`}>投稿中</div>
+				<div className={`${styles.face} ${styles.left}`}>投稿中</div>
+				<div className={`${styles.face} ${styles.top}`}>投稿中</div>
+				<div className={`${styles.face} ${styles.bottom}`}>投稿中</div>				
+			</div>
 		</div>
 	);
 }
