4d8bf384b6d00285fdd33986001eaa62c0b48cc1
aqizakana <archifish56@gmail.com>
2025-01-13 16:40:26 +0900

    fix css名変更

--- components/MessagePlate/MessagePlate.tsx
+++ components/MessagePlate/MessagePlate.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from "./messagePlate.module.css";
+import styles from "./MessagePlate.module.css";
 
 type MessageRecordItem = {
 	content: string;
