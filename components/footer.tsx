e65321a8afe44174157538b747fc9a6b2dbdc192
aqizakana <archifish56@gmail.com>
2025-01-16 00:21:19 +0900

    fix トップページレイアウト調整

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import style from "./footer.module.css";
 export function Footer() {
 	return (
 		<footer className={style.footer}>
-			<p className={style.footer}>© 2024 Shinnosuke Uota</p>
+			<p className={style.text}>© 2024 Shinnosuke Uota</p>
 		</footer>
 	);
 }
--- src/app/baseLayout.tsx
+++ src/app/baseLayout.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
-import Layout from "./layout";
 import "./globals.css";
 
 export default function BaseLayout({
